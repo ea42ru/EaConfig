bd86505635e67c6c2a400061754bc1927b86b9ea
Alexey Evtyushkin <ea42ru@mail.ru>
2021-09-28 15:20:21 +0300

    Test

--- src/ru/ea42/EaConfig/Main.java
+++ src/ru/ea42/EaConfig/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Config conf = new Config();
         System.out.println(conf.jsStr);
-        System.out.println("Test =>" + conf.getParamAsString("Test"));
+        System.out.println("Test => " + conf.getParamAsString("Test"));
+        System.out.println("Test1.User => " + conf.getParamAsString("Test1", "User"));
     }
 }
